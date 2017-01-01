3ebebd9b6ccd25e98887efd2226662977a47fd14
liao <461389537@qq.com>
2017-01-01 13:16:59 +0800

    adf

---
+++ app/controllers/account/posts_controller.rb
@@ -0,0 +1,12 @@
+class Account::PostsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @posts = current_user.posts
+  end
+
+
+private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,51 @@
+class PostsController < ApplicationController
+
+  before_filter :authenticate_user!, :only => [:new, :create]
+
+
+
+
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to account_posts_path
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path
+  end
+
+
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+end
